20720057fa3ac5fc24b68ec63472e6ced2ebb526
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2024-09-22 21:50:46 +0200

    End of section 16

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey:
+    'pk_test_51PyrH4B7ZzNSeMt5tHVEAsmEZSiEHlLQSGePmsgmgkQEmTIK3grFMaoKp9HYu6EDWz4A7UGHLjDgP8N7RsDYok8O00upQZvMtB',
 };
